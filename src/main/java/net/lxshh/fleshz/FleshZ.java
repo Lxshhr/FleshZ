6b870be1f1abe77c74c1d1f687836cd388d88e1b
Acey <yxshhp@gmail.com>
2025-10-30 21:17:03 +0300

    Initial Commit

---
+++ src/main/java/net/lxshh/fleshz/FleshZ.java
@@ -0,0 +1,20 @@
+package net.lxshh.fleshz;
+
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+@Mod(FleshZ.MODID)
+public class FleshZ
+{
+    public static final String MODID = "fleshz";
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public FleshZ() {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+}
