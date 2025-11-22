c26cff8467d89fa1a8a7d29bf7d294156c556ad5
Acey <yxshhp@gmail.com>
2025-11-22 17:55:25 +0300

    Add missing tag

--- src/main/java/net/lxshh/fleshz/datagen/BlockTagProvider.java
+++ src/main/java/net/lxshh/fleshz/datagen/BlockTagProvider.java
@@ -4,6 +4,7 @@ import net.lxshh.fleshz.common.ModTags;
 import net.lxshh.fleshz.common.blocks.ModBlocks;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.PackOutput;
+import net.minecraft.tags.BlockTags;
 import net.neoforged.neoforge.common.data.BlockTagsProvider;
 import net.neoforged.neoforge.common.data.ExistingFileHelper;
 import org.jetbrains.annotations.Nullable;
@@ -18,6 +19,17 @@ public class BlockTagProvider extends BlockTagsProvider {
     @Override
     protected void addTags(HolderLookup.Provider provider) {
         tag(ModTags.Blocks.RACKS)
+                .add(ModBlocks.ACACIA_WOOD_RACK.get())
+                .add(ModBlocks.OAK_WOOD_RACK.get())
+                .add(ModBlocks.DARK_OAK_WOOD_RACK.get())
+                .add(ModBlocks.SPRUCE_WOOD_RACK.get())
+                .add(ModBlocks.BIRCH_WOOD_RACK.get())
+                .add(ModBlocks.MANGROVE_WOOD_RACK.get())
+                .add(ModBlocks.BAMBOO_WOOD_RACK.get())
+                .add(ModBlocks.CHERRY_WOOD_RACK.get())
+                .add(ModBlocks.JUNGLE_WOOD_RACK.get());
+
+        tag(BlockTags.MINEABLE_WITH_AXE)
                 .add(ModBlocks.ACACIA_WOOD_RACK.get())
                 .add(ModBlocks.OAK_WOOD_RACK.get())
                 .add(ModBlocks.DARK_OAK_WOOD_RACK.get())
